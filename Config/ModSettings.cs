d40b20a6de90ff177c7e133a86a3d6795f09122f
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-11-09 21:39:20 -0600

    Removed some old comments in ModSettings

--- Config/ModSettings.cs
+++ Config/ModSettings.cs
@@ -16,37 +16,27 @@ namespace HGRework.Config
 
 		[Header("Pyramids")]
 
-		//[Label("Minimum Pyramids")]
-		//[Tooltip("The minimum number of pyramids that can generate in a world. (Not guaranteed, but it will try)")]
 		[DefaultValue(1)]
 		[Range(0, 3)]
 		[Increment(1)]
 		[Slider]
 		public int MinPyramids;
 
-		//[Label("Maximum Pyramids")]
-		//[Tooltip("The maximum number of pyramids that can generate in a world.")]
 		[DefaultValue(3)]
 		[Range(0, 3)]
 		[Increment(1)]
 		[Slider]
 		public int MaxPyramids;
 
-		//[Label("Rework Pyramids")]
-		//[Tooltip("Whether or not to rework pyramid generation to have more rooms")]
 		[DefaultValue(true)]
 		public bool ReworkPyramids;
 
-		//[Label("Minimum Pyramid Rooms")]
-		//[Tooltip("The minimum number of rooms that can generate in a pyramid. (Rework Pyramids must be enabled)")]
 		[DefaultValue(2)]
 		[Range(1, 10)]
 		[Increment(1)]
 		[Slider]
 		public int MinPyramidRooms;
 
-		//[Label("Maximum Pyramid Rooms")]
-		//[Tooltip("The maximum number of rooms that can generate in a pyramid. (Rework Pyramids must be enabled)")]
 		[DefaultValue(3)]
 		[Range(1, 10)]
 		[Increment(1)]
@@ -55,13 +45,9 @@ namespace HGRework.Config
 
 		[Header("Variants")]
 
-		//[Label("Both Evils")]
-		//[Tooltip("Whether or not to enable having both corruption and crimson in a world")]
 		[DefaultValue(true)]
 		public bool BothEvils;
 
-		//[Label("Both Ores")]
-		//[Tooltip("Whether or not to enable having both ore variants in a world")]
 		[DefaultValue(true)]
 		public bool BothOres;
 	}
